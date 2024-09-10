94fb7716fd440a9383fa8dc2b95b7560c1457a50
Gregory Brown <brown.gregm@gmail.com>
2024-09-10 11:17:50 -0700

    remove intents/issues from messages instead of logs

--- melodi/api.py
+++ melodi/api.py
@@ -403,18 +403,18 @@ class MelodiClient:
         except MelodiAPIError as e:
             raise MelodiAPIError(e)
 
-    def remove_issue_from_message(self, issue_id: int, log_id: int) -> None:
-        log = self.get_log(log_id)
+    def remove_issue_from_message(self, issue_id: int, message_id: int) -> None:
+        message = self.get_message(message_id)
 
-        for issue_association in log.issueAssociations:
+        for issue_association in message.issueAssociations:
             if (issue_association.issueId == issue_id):
-                issue_log_association_id = issue_association.id
+                issue_message_association_id = issue_association.id
                 break
 
-        if not issue_log_association_id:
-            raise MelodiAPIError(f"Issue {issue_id} is not associated to log {log_id}")
+        if not issue_message_association_id:
+            raise MelodiAPIError(f"Issue {issue_id} is not associated to message {message_id}")
 
-        url = f"{self.issue_log_associations_base_endpoint}/{issue_log_association_id}?apiKey={self.api_key}"
+        url = f"{self.issue_message_associations_base_endpoint}/{issue_message_association_id}?apiKey={self.api_key}"
 
         try:
             response = requests.delete(
@@ -426,7 +426,7 @@ class MelodiClient:
         except MelodiAPIError as e:
             raise MelodiAPIError(e)
 
-    def add_intent_to_log(self, intent_id: int, message_id: int) -> IntentMessageAssociation:
+    def add_intent_to_message(self, intent_id: int, message_id: int) -> IntentMessageAssociation:
         url = self.intent_message_associations_endpoint
 
         try:
@@ -440,18 +440,18 @@ class MelodiClient:
         except MelodiAPIError as e:
             raise MelodiAPIError(e)
 
-    def remove_intent_from_log(self, intent_id: int, log_id: int) -> None:
-        log = self.get_log(log_id)
+    def remove_intent_from_message(self, intent_id: int, message_id: int) -> None:
+        message = self.get_message(message_id)
 
-        for intent_association in log.intentAssociations:
+        for intent_association in message.intentAssociations:
             if (intent_association.intentId == intent_id):
-                intent_log_association_id = intent_association.id
+                intent_message_association_id = intent_association.id
                 break
 
-        if not intent_log_association_id:
-            raise MelodiAPIError(f"Intent {intent_id} is not associated to log {log_id}")
+        if not intent_message_association_id:
+            raise MelodiAPIError(f"Intent {intent_id} is not associated to log {message_id}")
 
-        url = f"{self.intent_log_associations_base_endpoint}/{intent_log_association_id}?apiKey={self.api_key}"
+        url = f"{self.intent_message_associations_base_endpoint}/{intent_message_association_id}?apiKey={self.api_key}"
 
         try:
             response = requests.delete(
