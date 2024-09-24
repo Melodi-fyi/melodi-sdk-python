0818ec930ef57e40b5d1a835d7ace9481a653e6d
Gregory Brown <brown.gregm@gmail.com>
2024-09-24 15:42:40 -0700

    update the openai utils to work with the refactor

--- melodi/utils/openai.py
+++ melodi/utils/openai.py
@@ -1,8 +1,12 @@
-from ..data_models import (ExternalUser, Log, LogInput, LogOutput, Message,
-                           Thread)
 
 
-def thread_object_from_open_ai_messages_response(messages_response, project_id: int, external_user: ExternalUser = None, metadata: dict = {}) -> Log:
+
+from melodi.messages.data_models import Message
+from melodi.threads.data_models import Thread
+from melodi.users.data_models import User
+
+
+def thread_object_from_open_ai_messages_response(messages_response, project_id: int, user: User = None, metadata: dict = {}) -> Thread:
   messages = messages_response.data
 
   melodi_messages = []
@@ -24,48 +28,7 @@ def thread_object_from_open_ai_messages_response(messages_response, project_id: 
       externalId=thread_id,
       messages=melodi_messages,
       metadata=metadata,
-      externalUser=external_user,
+      externalUser=user,
   )
 
   return thread
-
-def log_object_from_open_ai_messages_response(messages_response, project_id: int, external_user: ExternalUser = None, metadata: dict = {}) -> Log:
-  messages = messages_response.data
-
-  input_messages = []
-  for index, message in enumerate(messages):
-      if index == 0:
-          thread_id = message.thread_id
-          output_message = Message(
-              externalId=message.id,
-              role=message.role,
-              content=message.content[0].text.value,
-          )
-      else:
-          input_messages.append(
-              Message(
-                  externalId=message.id,
-                  role=message.role,
-                  content=message.content[0].text.value,
-              )
-          )
-
-  log = Log(
-      projectId=project_id,
-      externalThreadId=thread_id,
-
-      input=LogInput(
-          type="messages",
-          messages=input_messages,
-      ),
-      output=LogOutput(
-          type="message",
-          message=output_message,
-      ),
-
-      metadata=metadata,
-
-      externalUser=external_user,
-  )
-
-  return log
