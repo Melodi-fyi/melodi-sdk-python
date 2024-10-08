6fed1260cb2f096b8ca5a020d894ba60e4ff42a0
Gregory Brown <brown.gregm@gmail.com>
2024-10-08 14:46:44 -0700

    a fix for logging errors better

--- melodi/logging.py
+++ melodi/logging.py
@@ -1,4 +1,5 @@
 from logging import Logger
+
 from requests import Response
 
 
@@ -6,8 +7,10 @@ def _log_melodi_http_errors(logger: Logger, response: Response):
     if (response.status_code == 400):
         try:
             responseJson = response.json()
-            if (responseJson["errors"]):
+            if ("errors" in responseJson):
                 for error in responseJson["errors"]:
                     logger.error(f"Bad Request response from Melodi API: {error}")
+            else:
+                logger.error(f"Bad Request response from Melodi API: {responseJson}")
         except:
             pass
