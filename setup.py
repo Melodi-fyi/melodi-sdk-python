e3138c601eee54eeee685e1a2bec2bda2acb749f
Gregory Brown <brown.gregm@gmail.com>
2024-11-12 11:23:12 -0800

    dont included isDeleted field on feedback response

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='melodi',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'requests',
