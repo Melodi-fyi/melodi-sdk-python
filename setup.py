8086db699498691867eba24a7a2843cd7f8af004
Gregory Brown <brown.gregm@gmail.com>
2024-12-10 12:02:10 -0800

    separate Feedback and FeedbackResponse types to make them nicer and easier to use

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='melodi',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(),
     install_requires=[
         'requests',
