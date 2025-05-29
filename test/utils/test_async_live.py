8837048cc305f8edff8a63f80cfd0059f75d339d
amaiereanu <alexandru.maiereanu@intentsify.io>
2025-05-29 14:40:46 +0200

    OpenAI wrapper - add more async tests

--- test/utils/test_async_live.py
+++ test/utils/test_async_live.py
@@ -54,6 +54,81 @@ class TestAsyncWrapper(IsolatedAsyncioTestCase):
             # print(event)
             pass
 
+    @unittest.skip
+    async def test_azure_openai_streaming_function_tools(self):
+        client = AsyncAzureOpenAI(
+            api_key=os.getenv("AZURE_API_KEY"),
+            api_version="2025-03-01-preview",
+            azure_endpoint=os.getenv("AZURE_ENDPOINT"),
+        )
+
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather in a given location",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                        },
+                        "required": ["location"],
+                    },
+                }
+            }
+        ]
+
+        stream = await client.chat.completions.create(
+            model="gpt-4.1",
+            messages=[{"role": "user", "content": "What's the weather like in Brussels today?"}],
+            tools=tools,
+            stream=True
+        )
+
+        async for event in stream:
+            pass
+
+    # @unittest.skip
+    async def test_azure_openai_function_tools(self):
+        client = AsyncAzureOpenAI(
+            api_key=os.getenv("AZURE_API_KEY"),
+            api_version="2024-12-01-preview",
+            azure_endpoint=os.getenv("AZURE_ENDPOINT"),
+        )
+
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather in a given location",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                        },
+                        "required": ["location"],
+                    },
+                }
+            }
+        ]
+        messages = [{"role": "user", "content": "What's the weather like in Athens today?"}]
+        completion = await client.chat.completions.create(
+            model="gpt-4.1",
+            messages=messages,
+            tools=tools,
+            tool_choice="auto"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
