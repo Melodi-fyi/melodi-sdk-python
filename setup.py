0dc4d341138c842e4f467200f00753cc6c089463
Gregory Brown <brown.gregm@gmail.com>
2025-06-02 17:51:11 -0700

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='melodi',
-    version='0.1.34',
+    version='0.1.35',
     packages=find_packages(),
     install_requires=[
         'requests',
